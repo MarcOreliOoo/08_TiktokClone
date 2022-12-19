fc1ad1faf2c17b206b519c494e501f0b6c433f95
MarcOreliOoo <albeaurelien@gmail.com>
2022-12-19 11:01:43 +0100

    init: index.tsx for main page + sanity.io conf

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { footerList1, footerList2, footerList3 } from '../utils/constants';
+
+const List = ({ items, mt=true }: { items: string[], mt?: boolean }) => (
+	<div className={`flex flex-wrap gap-2 ${mt && 'mt-5'}`}>
+		{items.map((item) => (
+			<p key={item} className='text-gray-400 text-sm hover:underline cursor-pointer'>
+				{item}
+			</p>
+		))}
+	</div>
+);
 
 export const Footer = () => {
   return (
-	<div>Footer</div>
+	<div className='mt-6 hidden xl:block'>
+		<List items={footerList1} mt={false}/>
+		<List items={footerList2} />
+		<List items={footerList3} />
+		<p className='text-sm text-gray-400 mt-5'>2022 DoupsiTikTok</p>
+	</div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
 import type { NextPage } from 'next';
+import axios from 'axios';
 
 const Home: NextPage = () => {
 	return (
-		<></>
+		<>blabla</>
 	)
 }
 
+export const getServerSideProps = async() => {
+	const response = await axios.get(`http://localhost:3000/api/post`);	
+	
+	console.log(response.data.name);
+
+	return {
+		props: {}
+	}
+}
+
 export default Home;
+
