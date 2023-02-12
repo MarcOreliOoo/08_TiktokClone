880669facdba9a3a92c66eeac46a1a05bef901af
MarcOreliOoo <albeaurelien@gmail.com>
2023-02-12 19:14:18 +0100

    upd: detail page

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -9,6 +9,9 @@ import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
 import axios from 'axios';
 import { BASE_URL } from '../../utils/index';
 import { Video } from '../../types';
+import useAuthStore from '../../store/authStore';
+import LikeButton from '../../components/LikeButton';
+import Comments from '../../components/Comments';
 
 interface IProps {
 	postDetails: Video
@@ -19,6 +22,8 @@ const Detail = ({ postDetails }:IProps) => {
 	const [playing, setPlaying] = useState(false);
 	const [isVideoMuted, setIsVideoMuted] = useState(false);
 	const videoRef = useRef<HTMLVideoElement>(null);
+	const router = useRouter();
+	const userProfile = useAuthStore();
 
 	const onVideoClick = () => {
 		if(playing){
@@ -41,10 +46,10 @@ const Detail = ({ postDetails }:IProps) => {
 	return (
 		<div className='flex w-full absolute top-0 left-0 bg-white flex-wrap lg:flex-nowrap'>
 			<div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center'>
+				{/* Closing detail and router.back() */}
 				<div className='absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
-					<p>
+					<p className='cursor-pointer' onClick={() => router.back()}>
 						<MdOutlineCancel className='text-white text-[35px]' />
-						Detail
 					</p>
 				</div>
 				{/* Video view */}
@@ -80,6 +85,50 @@ const Detail = ({ postDetails }:IProps) => {
 							<HiVolumeUp className='text-black text-2xl lg:text-4xl'/>
 						</button>
 					)}
+				</div>
+			</div>
+
+			{/* Right part of the detail page */}
+			<div className='relative w-[1000px] md:w-[900px] lg:w-[700px]'>
+				<div className='lg:mt-20 mt-10'>
+					<div className='flex gap-3 cursor-pointer p-2 font-semibold rounded'>
+						{/* for profil pic on each post*/}
+						<div className='ml-4 md:w-20 md:h-20 w-16 h-16'>
+							<Link href="/">
+								<>
+									<Image width={62} height={62} className="rounded-full" src={post.postedBy.image} alt="profile pic" layout="responsive" />
+								</>
+							</Link>
+						</div>
+						{/* for profil name + verified status*/}
+						<div>
+							<Link href="/">
+								<div className='mt-3 flex flex-col gap-2'>
+									<p className='gap-2 flex md:text-md items-center font-bold text-primary'>
+										{post.postedBy.userName}{' '}<GoVerified className='text-blue-400 text-md' />
+									</p>
+									<p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>
+										{post.postedBy.userName}
+									</p>
+								</div>
+							</Link>
+						</div>
+					</div>
+					
+					{/*Video caption */}
+					<p className='px-10 text-lg text-gray-600'>
+						{post.caption}
+					</p>
+					
+					{/* Comment section */}
+					<div className='mt-10 px-10'>
+						{userProfile && (
+							<LikeButton />
+						)}
+					</div>
+					<Comments 
+					
+					/>
 				</div>
 			</div>
 		</div>
