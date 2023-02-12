d32dee023ce41acd46888b64e071365e757c66a4
MarcOreliOoo <albeaurelien@gmail.com>
2023-02-12 22:28:44 +0100

    upd: LikeButton component

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { MdFavorite } from 'react-icons/md';
+import useAuthStore from '../store/authStore';
+
 
 const LikeButton = () => {
-  return (
-	<div>LikeButton</div>
-  )
+	const [alreadyLiked, setAlreadyLiked] = useState(true);
+	const { userProfile } = useAuthStore();
+
+	return (
+		<div className='gap-6'>
+			<div className='mt-4 flex flex-col items-center justify-center cursor-pointer'>
+				{alreadyLiked ? (
+					<div className=''>
+						<MdFavorite className='text-lg md:text-2xl' />
+						37:31
+					</div>
+				):(
+					<div>
+						
+					</div>
+				)}
+				LikeButton
+			</div>
+			
+		</div>
+  	)
 }
 
 export default LikeButton
