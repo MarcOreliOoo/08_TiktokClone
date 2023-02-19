450da307d80c0a7b52967442fb17ce387223d2ef
MarcOreliOoo <albeaurelien@gmail.com>
2023-02-19 17:38:06 +0100

    feat: likeButton

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -2,25 +2,42 @@ import React, { useState, useEffect } from 'react';
 import { MdFavorite } from 'react-icons/md';
 import useAuthStore from '../store/authStore';
 
+interface IProps{
+	likes: any[];
+	handleLike: () => void;
+	handleDisLike: () => void;
+}
 
-const LikeButton = () => {
-	const [alreadyLiked, setAlreadyLiked] = useState(true);
-	const { userProfile } = useAuthStore();
+const LikeButton = ({ likes, handleLike, handleDisLike }:IProps) => {
+	const [alreadyLiked, setAlreadyLiked] = useState(false);
+	const { userProfile }: any = useAuthStore();
+
+	//We get all the likes and then filter to keep only those from our user
+	const filterLikes = likes?.filter((item) => item._ref === userProfile?._id);
+
+	useEffect(() => {
+		if(filterLikes?.length>0){
+			setAlreadyLiked(true);
+		} else {
+			setAlreadyLiked(false);
+		}
+	},[filterLikes,likes]);
 
 	return (
-		<div className='gap-6'>
+		<div className='flex gap-6'>
 			<div className='mt-4 flex flex-col items-center justify-center cursor-pointer'>
 				{alreadyLiked ? (
-					<div className=''>
+					<div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997]'
+						onClick={handleDisLike}>
 						<MdFavorite className='text-lg md:text-2xl' />
-						37:31
 					</div>
 				):(
-					<div>
-						
+					<div className='bg-primary rounded-full p-2 md:p-4'
+						onClick={handleLike}>
+						<MdFavorite className='text-lg md:text-2xl' />
 					</div>
 				)}
-				LikeButton
+				<p className='text-md font-semibold'>{likes?.length || 0}</p>
 			</div>
 			
 		</div>
--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -23,7 +23,7 @@ const Detail = ({ postDetails }:IProps) => {
 	const [isVideoMuted, setIsVideoMuted] = useState(false);
 	const videoRef = useRef<HTMLVideoElement>(null);
 	const router = useRouter();
-	const userProfile = useAuthStore();
+	const { userProfile }:any = useAuthStore();
 
 	const onVideoClick = () => {
 		if(playing){
@@ -40,6 +40,21 @@ const Detail = ({ postDetails }:IProps) => {
 			videoRef.current.muted = isVideoMuted;
 		}
 	},[post, isVideoMuted]);
+
+	const handleLike = async (like: boolean) => {
+		if(userProfile){
+			const { data } = await axios.put(`${BASE_URL}/api/like`, {
+				userId: userProfile._id,
+				postId: post._id,
+				like
+			});
+			//set of an object so we need:
+			//1- to open up the new object
+			//2- to spread the previous state of the object
+			//3- to select the property which we want to update
+			setPost({...post, likes: data.likes});
+		}
+	}
 
 	if(!post) return null;
 
@@ -123,7 +138,10 @@ const Detail = ({ postDetails }:IProps) => {
 					{/* Comment section */}
 					<div className='mt-10 px-10'>
 						{userProfile && (
-							<LikeButton />
+							<LikeButton 
+								likes={post.likes}
+								handleLike={() => handleLike(true)}
+								handleDisLike={() => handleLike(false)} />
 						)}
 					</div>
 					<Comments 
